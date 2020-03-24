18c2bce4bfaf7c9a5fdd0ecd0e3a5258a211d17b
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-03-24 18:08:55 -0300

    PROY 9 -115. Validando el Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
 
 const Formulario = ({}) => {
-  // State del Formulario
-
   const [busqueda, guardarBusqueda] = useState({
     ciudad: "",
     pais: ""
   });
 
-  // Extraer ciudad y pais, para colocarlos en el value
+  const [error, guardarError] = useState(false);
 
+  // Extraer ciudad y pais, para colocarlos en el value
   const { ciudad, pais } = busqueda;
 
-  // funcion que coloca los elementos en el State
+  // función que coloca los elementos en el state
   const handleChange = e => {
-    //Actualizar el State
+    // actualizar el state
     guardarBusqueda({
       ...busqueda,
       [e.target.name]: e.target.value
     });
   };
 
+  // Cuando el usuario da submit al form
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // validar
+    if (ciudad.trim() === "" || pais.trim() === "") {
+      guardarError(true);
+      return;
+    }
+
+    guardarError(false);
+
+    // Pasarlo al Componente
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {error ? (
+        <p className="red darken-4 error">Todos la Campos son Obligatorios</p>
+      ) : null}
+
       <div className="input-field col s12">
-        {/* name="ciudad" > para poder leerlo del state */}
         <input
           type="text"
           name="ciudad"
@@ -32,11 +49,12 @@ const Formulario = ({}) => {
           value={ciudad}
           onChange={handleChange}
         />
-        <label htmlFor="ciudad">Ciudad:</label>
+        <label htmlFor="ciudad">Ciudad: </label>
       </div>
+
       <div className="input-field col s12">
         <select name="pais" id="pais" value={pais} onChange={handleChange}>
-          <option value="">--Selecione un País--</option>
+          <option value="">-- Seleccione un país --</option>
           <option value="US">Estados Unidos</option>
           <option value="MX">México</option>
           <option value="CL">Chile</option>
@@ -46,7 +64,15 @@ const Formulario = ({}) => {
           <option value="ES">España</option>
           <option value="PE">Perú</option>
         </select>
-        <label htmlFor="pais">País:</label>
+        <label htmlFor="pais">País: </label>
+      </div>
+
+      <div className="input-field col s12">
+        <input
+          type="submit"
+          value="Buscar Clima"
+          className="waves-effect waves-light btn-large btn-block yellow accent-4"
+        />
       </div>
     </form>
   );
